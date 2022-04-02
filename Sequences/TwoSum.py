9d1c6508262fcfba496de5163ba33bb236e06a56
Jacob Law Zhen <69101485+thesneakerhead@users.noreply.github.com>
2022-04-03 01:03:31 +0800

    Update TwoSum.py

--- Sequences/TwoSum.py
+++ Sequences/TwoSum.py
@@ -7,8 +7,10 @@ def twoSum(self, nums, target):
         prevHash = {}
         for i, n in enumerate(nums):
             diff = target -n
+        # Check if number has already been iterated over and return index that is stored in dictionary (if it has)
             if diff in prevHash:
                 return [prevHash[diff], i]
+         # Else store current number value, index in the history of iterated numbers
             prevHash[n] = i
         return
         
